fb98f352dfe05df22af13e74b9e8c02f3614d9c1
Arrey-Etta <33989423+okhot@users.noreply.github.com>
2023-03-05 17:29:51 +0100

    update file structure

--- src/component/cat-card/cat-card.jsx
+++ src/component/cat-card/cat-card.jsx
@@ -20,12 +20,11 @@ export default function CatCard({ cardData }) {
     >
       <div className={style.card__image}></div>
       <div className={style.card__details}>
-        <h1 className={style.card__details__name}>Name:{name}</h1>
+        <h1 className={style.card__details__name}>Name:{name}</h1>{" "}
         <p>Origin: {origin}</p>
         <p>Child Friendly: {child_friendly}</p>
         <p>Dog Friendly: {dog_friendly}</p>
         <p>Stranger Friendly:{stranger_friendly}</p>
-        <button>Read More</button>
       </div>
     </div>
   );
